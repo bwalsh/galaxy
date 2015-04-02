70fcd25c285f472147db036d113bf492a2748234
psfoley <psfoley@gmail.com>
2015-04-02 14:08:25 -0700

    Code creates unique output directory for each run + throws exit(1) if wrong command line parameters are used

--- tools/cellprofiler/runcp.py
+++ tools/cellprofiler/runcp.py
@@ -1,20 +1,28 @@
 import subprocess
 import sys
 import os
+from random import randint
 
 DEVNULL = open(os.devnull,'wb') #This is needed to supress ilastik error due to running CellProfiler in headless configuration
 cp_path = os.environ['CP_PATH']
+outpath = str(randint(1,100000))
+tmppath = cp_path + '/bcc/' + outpath 
 if(sys.argv > 3):
 	print "Too many command line arguments"
 if(sys.argv[1] == 'bcc'):
-	os.chdir(cp_path+'/bcc/')
+	subprocess.call('mkdir '+tmppath,shell=True)
+	subprocess.call('mkdir '+tmppath+'/output',shell=True)
+	os.chdir(tmppath)
+	subprocess.call('ln -s ../* '+tmppath, shell=True)
 	subprocess.call('python createSymLinks.py',shell=True)
 	subprocess.call('python createFileList.py',shell=True)
 	subprocess.call('python makeHeadless.py',shell=True)
-	subprocess.call('cellprofiler -c -p headless.cppipe -i '+cp_path+'/bcc/ -o '+cp_path+'/bcc/output --data-file imageList.csv',shell=True,stderr=DEVNULL)
+	subprocess.call('cellprofiler -c -p headless.cppipe -i '+cp_path+'/bcc/ -o '+tmppath+'/output --data-file imageList.csv',shell=True,stderr=DEVNULL)
 	subprocess.call('python curateTSV.py '+sys.argv[2],shell=True)
 	subprocess.call('rm headless.cppipe',shell=True)
 	subprocess.call('python removeSymLinks.py',shell=True)
+	os.chdir(cp_path + '/bcc/')
+	subprocess.call('rm -rf '+outpath,shell=True)
 elif(sys.argv[1] == 'corless'):#existing pipeline filters out images (regex needs to converted to LoadImages Module)
 	os.chdir(cp_path + '/Corless_pipeline/')
 	subprocess.call('python createSymLinks.py',shell=True)
@@ -28,5 +36,5 @@ elif(sys.argv[1] == 'taka'):
 	subprocess.call('python makeHeadless.py',shell=True)
 	subprocess.call('cellprofiler -c -p headless.cppipe -i '+cp_path+'/Takahiro_Flow_Cytometry/ -o '+cp_path+'/Takahiro_Flow_Cytometry/output --data-file imageList.csv',shell=True)
 else:
-	System.out.println("Pipeline has not been implemented. Use Brenden Colson Center instead");
-
+	print "Pipeline has not been implemented. Use Brenden Colson Center instead"
+	exit(1)
